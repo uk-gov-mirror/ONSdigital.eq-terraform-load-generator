eb8da0ce32b1aabe12d81db5559aa4b20f279333
petechd <piotr.chudziak@ons.gov.uk>
2026-04-13 11:28:32 +0100

    Remove storage rm command

--- scripts/destroy_infrastructure.sh
+++ scripts/destroy_infrastructure.sh
@@ -6,9 +6,6 @@ TERRAFORM_STATE_BUCKET="${TERRAFORM_STATE_BUCKET:-eq-terraform-load-generator-tf
 
 terraform init --upgrade --backend-config prefix="${PROJECT_ID}" --backend-config bucket="${TERRAFORM_STATE_BUCKET}"
 
-# Do not delete the bucket.
-terraform state rm google_storage_bucket.benchmark-output-storage
-
 # Destroy infrastructure
 terraform destroy --auto-approve -var "project_id=${PROJECT_ID}"
 
