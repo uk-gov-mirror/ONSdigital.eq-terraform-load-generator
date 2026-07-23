0ee163a75d6c80209d71ba43fabda42d22e5b2c1
Abdurahman Abukar <77591967+a-abukar@users.noreply.github.com>
2026-07-23 13:01:05 +0100

    EQS-924: Upgrade google + google-beta provider to v7 (#35)

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 6.29.0, < 7.0.0"
+      version = ">= 7.0.0, < 8.0.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 6.29.0, < 7.0.0"
+      version = ">= 7.0.0, < 8.0.0"
     }
     random = {
       source  = "hashicorp/random"
